fde28d6a43be75a493f873679610d7c5965e6d4a
Vincent Ortega <Vincent Ortega>
2017-01-24 10:54:24 +0800

    Hello World Trial

---
+++ application/views/home.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Created by PhpStorm.
+ * User: Vincent
+ * Date: 01/24/2017
+ * Time: 10:49
+ */
+
+?><!DOCTYPE html>
+
+<html>
+    <head>
+
+    </head>
+    <body>
+        Hello World!
+    </body>
+</html>
