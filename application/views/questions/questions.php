a891becc50796a54622424c54c1fd46e43795303
NOODLECOMPUTER\Dante <cristina_aquino@dlsu.edu.ph>
2017-02-02 16:14:25 +0800

    stars work, generated list element has the id of the question num

--- application/views/questions/questions.php
+++ application/views/questions/questions.php
@@ -13,6 +13,13 @@
 
     $(document).on('ready', function(){
         $questions = <?php echo json_encode($questions)?>;
+        console.log($questions);
+
+        $('.rating-loading').rating({
+            step: 1,
+            starCaptions: {1: 'Very Poor', 2: 'Poor', 3: 'Ok', 4: 'Good', 5: 'Very Good'},
+            starCaptionClasses: {1: 'text-danger', 2: 'text-warning', 3: 'text-info', 4: 'text-primary', 5: 'text-success'}
+        });
     });
 
     function getNextQuestion(){
@@ -20,15 +27,29 @@
             $questions[$questionIndex]['Question_Act']
         ];
 
+        var id = [
+            $questions[$questionIndex]['Question_Num']
+        ];
+
         //allocate the div id later
         //does not scroll
         //TODO: make it scroll, probably related to some css shit
-        var newQuestion = '<li><div class="question"><p class="question-text">';
+        var newQuestion = '<li id="q';
+        newQuestion += id.join('');
+        newQuestion += '"><div class="question"><p class="question-text">';
         newQuestion += text.join('');
-        newQuestion += '</p></div></li>';
+        newQuestion += '</p>';
+        newQuestion += '<div class="question-stars">'+
+                        '<input id="star'+$questionIndex+'" name="input-name" type="number" class="rating-loading"></div>'
+        newQuestion += '</div></li>';
 
         $('.card-list').append(newQuestion);
         $questionIndex++;
+        $('.rating-loading').rating({
+            step: 1,
+            starCaptions: {1: 'Very Poor', 2: 'Poor', 3: 'Ok', 4: 'Good', 5: 'Very Good'},
+            starCaptionClasses: {1: 'text-danger', 2: 'text-warning', 3: 'text-info', 4: 'text-primary', 5: 'text-success'}
+        });
     }
 
 </script>
@@ -38,9 +59,8 @@
     <div class="main-card">
         <button class="up-button">up button</button>
         <ul class="card-list">
-            <li>
-                <div class="question" id="q1">
-<!--                    <div class="question-text">question and orange bg go here</div>-->
+            <li id="start">
+                <div class="question">
                     <p class="question-text"> This is the first question</p>
                     <div class="question-stars">stars and description text go here</div>
                 </div>
@@ -51,28 +71,3 @@
     <div class="main-progBar">this is a progress bar</div>
 </div>
 <footer>this is the footer and copyright info go here</footer>
-
-<!-- TODO: FOR REFERENCE DELETE LATER
-<section class="site-body">
-    <div class="body-questions">
-        <ul id="posts">
-            <li>
-                <div class="question-card">Wow a QUESTION!</div>
-            </li>
-        </ul>
-        <button class="next-button" onclick="clickFunction()">next</button>
-    </div>
-</section>
-
-<footer class="site-footer">
-    <div class="footer-copyright">
-        COPYRIGHT COPYRIGHT
-        <br>
-        PIRACY IS STEALING AND STEALING IS AGAINST THE LAW
-        <br>
-        AYYY!
-        <br>
-        <div class="link"><a>Home</a></div>
-    </div>
-</footer>
--->
