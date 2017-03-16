5bb0b00bec686de3c4645e188d64d2cabbaed45d
Vincent Ortega <Vincent Ortega>
2017-03-16 16:03:14 +0800

    font awesome fix

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
-//http://localhost/Praxis-Survey
+$config['base_url'] = 'http://localhost/Praxis-Survey';
+
 /*
 |--------------------------------------------------------------------------
 | Index File
