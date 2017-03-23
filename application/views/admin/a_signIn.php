4fcb46ff1bfb9f30210bb7d000299b60521685f2
Vincent Ortega <Vincent Ortega>
2017-03-23 11:26:54 +0800

    Logos

--- application/views/admin/a_signIn.php
+++ application/views/admin/a_signIn.php
@@ -1,20 +1,21 @@
-<form class="col-md-4 col-md-offset-4" method="post" action="<?=base_url('admin/' . ADMIN_SIGN_IN)?>">
+<form class="col-md-4 col-md-offset-4" method="post" action="<?=base_url('admin/' . ADMIN_SIGN_IN)?>" style="padding-top: 10%">
 
     <span id="error-message"><?=$errorMessage?></span>
+
+
+    <center><img src="<?=base_url()?>/assets/img/Praxis%20Logo.svg" style="width: 60%; height: auto;"></center>
 
     <div class="form-group">
         <label for="adminName">Username:</label>
         <input type="text" class="form-control" name="adminName" id="adminName" placeholder="Enter Username Here" required>
     </div>
 
-    <BR><BR>
 
     <div class="form-group">
         <label for="adminPassword">Password:</label>
         <input type="password" class="form-control" name="adminPassword" id="adminPassword" placeholder="Enter Your Password Here" required>
     </div>
 
-    <BR><BR>
 
 
     <button type="submit" class="btn btn-default" id="submit-sign-in">Sign In</button>
